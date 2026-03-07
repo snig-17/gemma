4c6a1658d8940566cc3d261549b88e7e944c28d0
snig-17 <snigdha.17.tiwari@icloud.com>
2026-03-07 15:24:15 +0000

    Initial Commit

---
+++ gemma/gemmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemmaApp.swift
+//  gemma
+//
+//  Created by Snigdha Tiwari  on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct gemmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
